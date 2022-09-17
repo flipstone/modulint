cf946a1283894ae9e57f843f382363b61d8278ac
Trevis Elser <trevis@flipstone.com>
2022-09-17 00:51:02 -0400

    wip
    WIP squash me down before merge
    
    WIP squash me down before merge
    
    Swap to a typechecker plugin to avoid some haddock oddities running
    typechecking during the parse step.
    
    Still needs to be squashed down before merging.
    
    Only silence stdout when creating haddock interfaces
    
    Be quiet with haddock via flags instead of via silently
    
    Don't run haddock stuff when global is set to check a min of 0, also squashme please
    
    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,122 @@ module Main
   ( main
   ) where
 
-import qualified Modulint.Main as Modulint
-import qualified Modulint.Options as Options
+import qualified Config.Initialize as Initialize
+import qualified Henforcer.Options as Options
 
 main :: IO ()
 main = do
-  options <- Options.parseOptions
-  Modulint.main options
+  opts <- Options.parseOptions
+  if Options.initialize opts
+    then Initialize.initializeConfigPath (Options.configPath opts)
+    else putStrLn "The only option currently supported for the executable usage is init."
 
+-- config <- Config.loadConfigFile (Options.configPath opts)
+-- runWithConfig config
+
+-- runWithConfig :: Config.Config -> IO ()
+-- runWithConfig cfg =
+--   GHC.defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
+--       GHC.runGhc (Just libdir) $ do
+--         dflags <- GHC.getSessionDynFlags
+--         GHC.setSessionDynFlags dflags
+--         explodedDirs <- GHC.liftIO $ traverseDir isHaskellFile $ Config.sourcePaths cfg
+--         newTargets <- traverse GHC.guessTargetForFile explodedDirs
+--         GHC.liftIO $ print explodedDirs
+--         GHC.setTargets newTargets
+
+--         graph <- GHC.depanal [] True
+--         let ic = ImportCheck.newImportChecker cfg
+--             docE = Config.allowedDocumentation cfg
+--             topoSorted = Maybe.catMaybes $ fmap GHC.moduleGraphNodeModSum . GHC.flattenSCCs $ GHC.topSortModuleGraph False graph Nothing
+
+--         -- GHC.liftIO $ print $ fmap GHC.ms_mod_name summaries
+--         GHC.liftIO $ print $ fmap GHC.ms_mod_name topoSorted
+--         haddockErrorsOrRuleFailures <- GHC.liftIO $ checkHaddock docE topoSorted
+
+--         importFailures <- traverse (checkImport ic) topoSorted
+
+--         case haddockErrorsOrRuleFailures of
+--           Left _ ->
+--             -- TODO: Handle errors
+--             GHC.liftIO $ print ""
+--           Right fails ->
+--             GHC.liftIO . handleFailures $ (fails <> join importFailures)
+
+-- checkImport :: ImportCheck.ImportChecker -> GHC.ModSummary -> GHC.Ghc [GHC.SDoc]
+-- checkImport ic summary = do
+--   parsedMod <- GHC.parseModule summary
+--   let
+--     hsMod = GHC.unLoc $ GHC.pm_parsed_source parsedMod
+
+--   pure . fmap ImportCheck.toSDoc $ ImportCheck.checkModule ic hsMod
+
+-- checkHaddock :: H.DocumentationRules -> [GHC.ModSummary] -> IO (Either H.HaddockError [GHC.SDoc])
+-- checkHaddock docE summaries = do
+--   ifaces <- H.silentlyCreateInterfaces' summaries
+--   case ifaces of
+--     Left err -> do
+--       print err
+--       pure $ pure []
+--     Right [] -> do
+--       putStrLn "No interfaces!"
+--       pure $ pure []
+--     Right xs -> do
+--       let
+--         errOrDocs = ((fmap GHC.ppr . H.checkInterfaces docE)) xs
+--       case errOrDocs of
+--         [] -> do
+--           putStrLn "No docs"
+--           pure $ pure errOrDocs
+--         docs -> do
+--           putStrLn "Docs"
+--           print docs
+--           pure $ pure errOrDocs
+
+-- handleFailures :: [GHC.SDoc] -> IO ()
+-- handleFailures failures =
+--   case length failures of
+--     0 -> do
+--       putStrLn "No errors found"
+--       Exit.exitSuccess
+--     errCount -> do
+--       putStr $ formatFailures failures
+--       putStrLn $ "\nhenforcer: " <> show errCount <> " errors found!"
+--       Exit.exitWith checksFailed
+
+-- isHaskellFile :: FilePath -> Bool
+-- isHaskellFile filePath =
+--   FilePath.takeExtension filePath == ".hs"
+
+-- checksFailed :: Exit.ExitCode
+-- checksFailed =
+--   Exit.ExitFailure 20
+
+-- formatFailures :: [GHC.SDoc] -> String
+-- formatFailures =
+--   GHC.renderWithDefaultContext . GHC.vcat
+
+-- traverseDir :: (FilePath -> Bool)
+--             -> [FilePath]
+--             -> IO [FilePath]
+-- traverseDir isFileOfInterest initial =
+--   let
+--     go accum baseDir = do
+--         contents <- Dir.listDirectory baseDir
+
+--         let
+--           fullPaths = fmap (FilePath.combine baseDir) contents
+
+--         Monad.foldM foldEntry accum fullPaths
+
+--     foldEntry accum path = do
+--         status <- Posix.getFileStatus path
+
+--         if Posix.isDirectory status then
+--           go accum path
+--         else if Posix.isRegularFile status && isFileOfInterest path then
+--           pure $ accum <> [path]
+--         else
+--           pure accum
+--   in
+--     fmap join $ traverse (go []) initial
